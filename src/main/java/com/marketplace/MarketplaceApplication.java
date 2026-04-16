af19761888faeec5b9f14d3413579e94789e009f
andyvin <joviandy2020@gmail.com>
2026-04-16 12:01:34 +0700

    fix dependency

--- src/main/java/com/marketplace/MarketplaceApplication.java
+++ src/main/java/com/marketplace/MarketplaceApplication.java
@@ -1,14 +1,14 @@
 package com.marketplace;
 
-import com.marketplace.Config.JjwtRuntimeHints;
+//import com.marketplace.Config.JjwtRuntimeHints;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
-import org.springframework.context.annotation.ImportRuntimeHints;
+//import org.springframework.context.annotation.ImportRuntimeHints;
 import org.springframework.context.annotation.ReflectiveScan;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-@ImportRuntimeHints(JjwtRuntimeHints.class)
+//@ImportRuntimeHints(JjwtRuntimeHints.class)
 @ReflectiveScan
 public class MarketplaceApplication {
 
