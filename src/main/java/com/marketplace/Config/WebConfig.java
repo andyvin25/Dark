670071fd8e4736ce5db37464bc87d3b9f352a751
andyvin <joviandy2020@gmail.com>
2026-04-26 16:27:46 +0700

    add url origin for allowed origins at webmvcconfigurer

--- src/main/java/com/marketplace/Config/WebConfig.java
+++ src/main/java/com/marketplace/Config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.marketplace.Config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -11,6 +12,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig {
+
+    @Value("${url_domain.allowed_origin}")
+    private String urlDomain;
+
     @Bean
     public WebMvcConfigurer cors() {
         return new WebMvcConfigurer() {
@@ -18,7 +23,7 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry
                         .addMapping("/**")
-                        .allowedOrigins("http://localhost:5173/")
+                        .allowedOrigins(urlDomain)
                         .allowedHeaders(HttpHeaders.CONTENT_TYPE, HttpHeaders.AUTHORIZATION)
 //                        .allowedHeaders("*")
                         .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(), HttpMethod.PATCH.name(), HttpMethod.DELETE.name(), HttpMethod.OPTIONS.name())
